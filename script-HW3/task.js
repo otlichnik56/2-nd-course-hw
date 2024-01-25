b648a8509810de80bd9514a66300ef57e0b2f66a
otlichnik56 <110157411+otlichnik56@users.noreply.github.com>
2024-01-25 09:24:10 +0500

    Update task.js

--- script-HW3/task.js
+++ script-HW3/task.js
@@ -32,24 +32,24 @@ alert(Number(a) + Number(b));
 // task 5
 let monthNumber = Number(prompt("Введите номер месяца"));
 switch (monthNumber) {
-    case 1:
+    	case 1:
 	case 2:
-    case 12:
+    	case 12:
 		alert("Зима же");
 		break;
 	case 3:
-    case 4:
-    case 5:
+    	case 4:
+    	case 5:
 		alert("Весна же");
 		break;
-    case 6:
-    case 7:
-    case 8:
+    	case 6:
+    	case 7:
+    	case 8:
 		alert("Лето же");
 		break;
-    case 9:
-    case 10:
-    case 11:
+    	case 9:
+    	case 10:
+    	case 11:
 		alert("Осень же");
 		break;
 	default: alert("Нет такого месяца");
