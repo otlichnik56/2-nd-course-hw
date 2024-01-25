16de793eeb4ac5be2aa86f90d7ce1ebde235bd5e
Hyp <otlichnik56@yandex.ru>
2024-01-25 12:03:39 +0500

    Дз 5 от 25.01.2024

---
+++ script-HW5/task.js
@@ -0,0 +1,109 @@
+console.log(findMinNumber(10, 2)); // Проверка 1 задачи
+console.log(findMinNumber(5, 5)); // Проверка 1 задачи
+console.log(checkEvenOrOdd(10)); // Проверка 2 задачи
+console.log(checkEvenOrOdd(7));  // Проверка 2 задачи
+printSquare(5);  // Проверка 3.1 задачи
+console.log(square(5)); // Проверка 3.2 задачи
+greetUser();  // Проверка 4 задачи
+console.log(multiplyNumbers(5, 3)); // Проверка 5 задачи
+console.log(multiplyNumbers('abc', 3)); // Проверка 5 задачи
+console.log(multiplyNumbers(7, 'xyz')); // Проверка 5 задачи
+console.log(cubeNumber());  // Проверка 6 задачи
+console.log(`Площадь круга circle1: ${circle1.getArea()}`);  // Проверка 7 задачи
+console.log(`Периметр круга circle1: ${circle1.getPerimeter()}`);  // Проверка 7 задачи
+console.log(`Площадь круга circle2: ${circle2.getArea()}`);  // Проверка 7 задачи
+console.log(`Периметр круга circle2: ${circle2.getPerimeter()}`);  // Проверка 7 задачи
+console.log(getSeason(1));  // Проверка 8 задачи
+console.log(getSeason(4));  // Проверка 8 задачи
+console.log(getSeason(8));  // Проверка 8 задачи
+console.log(getSeason(11)); // Проверка 8 задачи
+console.log(getSeason(13)); // Проверка 8 задачи
+
+
+// task 1
+findMinNumber = (a, b) => (a < b ? a : b); 
+
+
+// task 2
+const checkEvenOrOdd = (number) => (number % 2 === 0 ? 'Число четное' : 'Число нечетное');
+
+
+// task 3
+const printSquare = (number) => {
+    const square = number * number;
+    console.log(`Квадрат числа ${number} равен ${square}`);
+};
+const square = (number) => number * number;
+
+
+// task 4
+function greetUser() {
+    const age = parseInt(prompt("Сколько вам лет?"));    
+    if (isNaN(age) || age < 0) { 
+        console.log("Вы ввели неправильное значение");
+    } else if (age >= 0 && age <= 12) { 
+        console.log("Привет, друг!");
+    } else { 
+      console.log("Добро пожаловать!");
+    }
+}
+
+
+// task 5
+function multiplyNumbers(num1, num2) {
+    if (typeof num1 !== 'number' || typeof num2 !== 'number' || isNaN(num1) || isNaN(num2)) {
+        return 'Одно или оба значения не являются числом';
+    } else {
+        return num1 * num2;
+    }
+}
+
+
+// task 6
+function cubeNumber() {
+    const userInput = prompt("Введите число:");
+    const number = parseFloat(userInput);  
+    if (isNaN(number)) {
+        return 'Переданный параметр не является числом';
+    } else {
+        const result = number ** 3; 
+        return `${number} в кубе равняется ${result}`;
+    }
+}
+
+
+// task 7
+const circle1 = {
+    radius: 5, 
+    getArea: function() {
+        return Math.PI * this.radius ** 2; 
+    },
+    getPerimeter: function() {
+        return 2 * Math.PI * this.radius; 
+    }
+};
+const circle2 = {
+    radius: 8, 
+    getArea: function() {
+        return Math.PI * this.radius ** 2; 
+    },
+    getPerimeter: function() {
+        return 2 * Math.PI * this.radius; 
+    }
+};
+
+
+// task 8
+function getSeason(monthNumber) {
+    if (monthNumber === 12 || monthNumber <= 2) {
+        return "Зима";
+    } else if (monthNumber >= 3 && monthNumber <= 5) {
+        return "Весна";
+    } else if (monthNumber >= 6 && monthNumber <= 8) {
+        return "Лето";
+    } else if (monthNumber >= 9 && monthNumber <= 11) {
+        return "Осень";
+    } else {
+        return "Неправильный номер месяца";
+    }
+}
